0f167afa66cb0d292701bbad21cb1993ece31c15
mgoldchild <mgoldchild@gmail.com>
2019-10-10 15:42:22 +0900

    update main.py

--- applications/yolov2/main.py
+++ applications/yolov2/main.py
@@ -4,6 +4,8 @@ import onnx
 from keras.models import load_model
 from PIL import Image, ImageDraw, ImageFont
 
+ONNX_FILE_NAME = "onnx_yolov2_tiny_voc.onnx"
+
 
 def conv_model():
     target_opset = 8
@@ -11,7 +13,7 @@ def conv_model():
     onnx_model = keras2onnx.convert_keras(
         model=keras_model, target_opset=target_opset, channel_first_inputs="input_1"
     )
-    onnx.save_model(onnx_model, "onnx_yolov2_tiny_voc.onnx")
+    onnx.save_model(onnx_model, ONNX_FILE_NAME)
 
 
 def sigmoid(x, derivative=False):
@@ -29,20 +31,21 @@ def detect_img():
     - https://tech-blog.optim.co.jp/entry/2018/12/05/160831
     - https://qiita.com/miyamotok0105/items/1aa653512dd4657401db
     - https://machinethink.net/blog/object-detection-with-yolo/
+    - https://github.com/shi3z/onnx-example
     """
     import onnxruntime
 
-    sess = onnxruntime.InferenceSession("./tiny_yolov2/Model.onnx")
-    # sess = onnxruntime.InferenceSession("onnx_tiny_voc.onnx")
+    sess = onnxruntime.InferenceSession(ONNX_FILE_NAME)
     input_name = sess.get_inputs()[0].name
-    img = Image.open("dog.jpg")
+    img = Image.open("person.jpg")
     img = img.resize((416, 416))  # for tiny_yolov2
-    x = np.asarray(img)
-    x = x.transpose(2, 0, 1)
-    x = x.reshape(1, 3, 416, 416)
-    print(x.shape)
+    image_data = np.array(img, dtype="float32")
+    image_data /= 255.0
+    image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+    image_data = image_data.transpose(0, 3, 1, 2)
+    print(image_data.shape)
 
-    out = sess.run(None, {input_name: x.astype(np.float32)})
+    out = sess.run(None, {input_name: image_data.astype(np.float32)})
     out = out[0][0]
     print("shape", np.shape(out))
     numClasses = 20
@@ -103,16 +106,16 @@ def detect_img():
         for cx in range(0, 13):
             for b in range(0, bb_cnt):
                 channel = b * (numClasses + bb_cnt)
-                tx = out[channel][cy][cx]
-                ty = out[channel + 1][cy][cx]
-                tw = out[channel + 2][cy][cx]
-                th = out[channel + 3][cy][cx]
-                tc = out[channel + 4][cy][cx]
-                # tx = out[cy][cx][channel]
-                # ty = out[cy][cx][channel + 1]
-                # tw = out[cy][cx][channel + 2]
-                # th = out[cy][cx][channel + 3]
-                # tc = out[cy][cx][channel + 4]
+                # tx = out[channel][cy][cx]
+                # ty = out[channel + 1][cy][cx]
+                # tw = out[channel + 2][cy][cx]
+                # th = out[channel + 3][cy][cx]
+                # tc = out[channel + 4][cy][cx]
+                tx = out[cy][cx][channel]
+                ty = out[cy][cx][channel + 1]
+                tw = out[cy][cx][channel + 2]
+                th = out[cy][cx][channel + 3]
+                tc = out[cy][cx][channel + 4]
 
                 x = (float(cx) + sigmoid(tx)) * 32
                 y = (float(cy) + sigmoid(ty)) * 32
@@ -120,14 +123,12 @@ def detect_img():
                 w = np.exp(tw) * 32 * anchors[2 * b]
                 h = np.exp(th) * 32 * anchors[2 * b + 1]
 
-                # if tc < -1:
-                #     tc = -1
                 confidence = sigmoid(tc)
 
                 classes = np.zeros(numClasses)
                 for c in range(0, numClasses):
-                    classes[c] = out[channel + bb_cnt + c][cy][cx]
-                    # classes[c] = out[cy][cx][channel + bb_cnt + c]
+                    # classes[c] = out[channel + bb_cnt + c][cy][cx]
+                    classes[c] = out[cy][cx][channel + bb_cnt + c]
                 classes = softmax(classes)
                 detectedClass = classes.argmax()
 
